66ee09cf053bc6214384ed0d4ec80479b3451c2d
Atnanasi <ys2000pro@gmail.com>
2019-08-22 01:28:35 +0900

    feat: Add code

---
+++ src/comment.ts
@@ -0,0 +1,14 @@
+import User from './user'
+
+export default interface Comment {
+  url:                string;
+  html_url:           string;
+  issue_url:          string;
+  id:                 number;
+  node_id:            string;
+  user:               User;
+  created_at:         string;
+  updated_at:         string;
+  author_association: string;
+  body:               string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,88 @@
-console.log('hello')
+import { Feed } from 'feed'
+import axios from 'axios'
+import Koa from 'koa'
+import Router from 'koa-router'
+import { Base64 } from 'js-base64'
+
+import Comment from './comment';
+import Notification from './notification';
+import Pull from './pull';
+
+const app = new Koa()
+const router = new Router()
+
+const getToken = (header: string) => {
+  const body = header.split(' ')[1]
+  const base64 = Base64.decode(body)
+  const authorization = base64.toString().split(':')
+
+  return authorization[1]
+}
+
+const dateSort = (a:string, b:string) => {
+  const aDate = new Date(a)
+  const bDate = new Date(b)
+
+  return aDate == bDate ? 0 : aDate < bDate ? 1 : -1
+}
+
+router.get("/atom", async ctx => {
+  const token = getToken(ctx.request.headers['authorization'])
+
+  const notifications = await axios.get<Notification[]>('https://api.github.com/notifications', {
+    headers: {
+      "Authorization": `token ${token}`,
+    },
+  })
+
+  const feed = new Feed({
+    title: "GitHub Notifications",
+    description: "Depending on your notification settings, you’ll see updates here for your conversations in watched repositories.",
+    id: "https://ilmestys.now.sh",
+    link: "https://github.com/notifications",
+    image: "https://github.githubassets.com/pinned-octocat.svg",
+    favicon: "https://github.githubassets.com/favicon.ico",
+    copyright: "GitHub, Inc.",
+    updated: new Date(notifications.data.sort((a,b) => dateSort(a.updated_at, b.updated_at))[0].updated_at),
+    generator: "ilmestys",
+    author: {
+      name: "GitHub",
+      link: "https://github.com"
+    },
+  });
+
+  await Promise.all(notifications.data.map(async notification => {
+    const description = await axios.get<Comment|Pull>(
+      notification.subject.latest_comment_url || notification.subject.url
+    )
+
+    feed.addItem({
+      title: notification.subject.title,
+      id: notification.id,
+      link: notification.subject.url,
+      description: description.data.html_url,
+      content: description.data.body,
+      author: [
+        {
+          name: description.data.user.login,
+          link: description.data.user.html_url,
+        },
+      ],
+      date: new Date(notification.updated_at),
+      image: description.data.user.avatar_url,
+    })
+  }))
+
+  ctx.type = "application/atom+xml; charset=utf-8"
+  ctx.body = feed.atom1()
+})
+
+app.use(router.routes())
+
+if (!process.env.IS_NOW) {
+  const port = process.env.PORT || 5000
+  app.listen(port)
+  console.log(`Application has booted on http://localhost:${port}`)
+}
+
+export default app.callback()
